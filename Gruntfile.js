1f68692433b70a9a70981dde9257efdad6f6d4c3
yaoazhen <yaoazhen@hotmail.com>
2013-08-12 15:41:47 +0800

    add matchdep to load all grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -134,18 +134,8 @@ module.exports = function(grunt) {
   });
 
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('browserstack-runner');
-
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Test task.
   var testSubtasks = ['jshint', 'qunit'];
