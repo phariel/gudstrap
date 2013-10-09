51928e1ae87915a3843e15c1b08fdca8ec1541cc
Vince Liu <Vince.Liu@EF.com>
2013-10-09 02:53:47 -0400

    fixes englishtown/gudstrap#71

--- dist/js/gudstrap.js
+++ dist/js/gudstrap.js
@@ -2073,7 +2073,7 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
   var execStep = function($input, action, options, step) {
     if ($input.is(disableAttr)) return
     var val
-    if (step === undefined) step = options['step']
+    if (step === undefined) step = options.step
     if (isNaN(val = parseInt($input.val(), 10))) val = 0
     step = parseInt(step, 10)
 
@@ -2087,8 +2087,8 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
   }
 
   var setVal = function($input, val, options) {
-    var maxValue = parseInt(options['max'], 10)
-    var minValue = parseInt(options['min'], 10)
+    var maxValue = parseInt(options.max, 10)
+    var minValue = parseInt(options.min, 10)
 
     var lastVal = $input.data(dataLastVal)
 
